deb02b333b13c098d34e3bab4460227003035091
Johannes Sindlinger <gabriel.sindlinger@gmx.net>
2026-05-30 11:11:19 +0200

    fix sitemap

--- webpage/src/routes/sitemap.xml/+server.ts
+++ webpage/src/routes/sitemap.xml/+server.ts
@@ -1,18 +1,14 @@
 import { siteBaseUrl } from '$lib/data/meta';
-import { filterPosts, importPosts } from '$lib/data/blog-posts/utils';
 
 export const prerender = true;
 
 const staticRoutes = [
 	{ path: '', priority: '1.0', changefreq: 'weekly' },
-	{ path: 'about', priority: '0.8', changefreq: 'monthly' },
-	{ path: 'publications', priority: '0.8', changefreq: 'monthly' }
+	{ path: 'publications', priority: '0.8', changefreq: 'monthly' },
+	{ path: 'about', priority: '0.8', changefreq: 'monthly' }
 ];
 
 export async function GET() {
-	const allPosts = importPosts(false);
-	const posts = filterPosts(allPosts);
-
 	const base = siteBaseUrl.replace(/\/$/, '');
 
 	const staticEntries = staticRoutes
@@ -26,21 +22,9 @@ export async function GET() {
 		)
 		.join('');
 
-	const postEntries = posts
-		.map(
-			(post) => `
-  <url>
-    <loc>${base}/${post.slug}</loc>
-    <lastmod>${new Date(post.updated || post.date).toISOString().split('T')[0]}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.6</priority>
-  </url>`
-		)
-		.join('');
-
 	const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${staticEntries}${postEntries}
+${staticEntries}
 </urlset>`;
 
 	return new Response(body, {
